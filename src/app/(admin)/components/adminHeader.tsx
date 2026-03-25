01db478f7884c41d54e48db9e3b8b9498a08c051
Your Name <your.email@example.com>
2026-03-25 21:53:27 +0700

    feat: initial commit

--- src/app/(admin)/components/adminHeader.tsx
+++ src/app/(admin)/components/adminHeader.tsx
@@ -1,7 +1,7 @@
 const AdminHeader = () => {
   return (
     <header className="bg-white shadow">
-      <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+      <div className="py-6 px-4 sm:px-6 lg:px-8">
         <h1 className="text-3xl font-bold text-gray-900">Admin Dashboard</h1>
       </div>
     </header>

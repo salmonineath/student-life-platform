c95ab91a624ab62bda3345941d27bbc6e4813268
neathcmd <ksdf71464@gmail.com>
2026-03-26 14:25:12 +0700

    feat: initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, Geist } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
 
-const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
 
 export const metadata: Metadata = {
   title: "Landing page",
@@ -15,8 +16,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="scroll-smooth">
-      <body className={`${inter.variable} font-sans`}>{children}</body>
+    <html lang="en" className={cn("scroll-smooth", "font-sans", geist.variable)}>
+      <body className={`${geist.variable} font-sans`}>{children}</body>
     </html>
   );
 }

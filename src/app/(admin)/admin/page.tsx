6d2428bf4f40be1e5ccf9f25958ad1b329f25458
neathcmd <ksdf71464@gmail.com>
2026-03-27 07:52:47 +0700

    remove shadcn

--- src/app/(admin)/admin/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function AdminDashboard() {
-  return (
-    <div className="border-2 border-black min-h-screen">
-      <h1 className="text-black">Welcome to the Admin Dashboard</h1>
-    </div>
-  );
-}

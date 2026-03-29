6434e454614c5b30cfec542aaa6bebcf92a2d12d
Your Name <your.email@example.com>
2026-03-29 09:44:32 +0700

    bug not fixed

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,10 +1,9 @@
 import axios from "axios";
 
-// const API_URL =
-//   process.env.NEXT_PUBLIC_API_URL ||
-//   "http://localhost:5000/api/v1";
+const API_URL =
+  process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000/api/v1";
 
-const API_URL = "http://localhost:5000/api/v1";
+// const API_URL = "http://localhost:5000/api/v1";
 
 const axiosInstance = axios.create({
   baseURL: API_URL,

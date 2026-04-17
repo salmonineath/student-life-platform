c780109bcd3d8bf30ddfa9064c227ea6685c71a0
salmonineath <ksdf71464@gmail.com>
2026-04-17 15:47:59 +0700

    add warm up server function in landing page

--- src/app/(landing)/student-life/page.tsx
+++ src/app/(landing)/student-life/page.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { useEffect } from "react";
 import Footer from "../components/Footer";
 import HeroSection from "../components/HeroSection";
 import FeatureSection from "../components/FeatureSection";
@@ -7,9 +10,23 @@ import Header from "../components/Header";
 import BannerSection from "../components/BannerSection";
 import TrustBar from "../components/TrustBar";
 
+function WarmUpServer() {
+  const backendUrl = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000";
+
+  useEffect(() => {
+    fetch(`${backendUrl}`)
+      .then(() => console.log("Backend warmed up", backendUrl))
+      .catch((err) => console.error("Error warming up backend:", err));
+  }, []);
+
+  return null;
+}
+
 export default function LandingPage() {
   return (
     <div className="min-h-screen bg-white">
+      <WarmUpServer />
+
       {/* Navigation */}
       <Header />
 
